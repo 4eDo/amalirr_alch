ab190bf2469d013ab6d871691fdc08960433b1cf
4eDo <61490588+4eDo@users.noreply.github.com>
2022-06-18 20:49:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function dropMe(id, type, name, phys, magic, holy, def, atc) {
   updBoilerParams('holy', holy);
   updBoilerParams('def', def);
   updBoilerParams('atc', atc);
-  document.getElementById("count_").innerText =  parseInt(document.getElementById("count_" + id).innerText, 10) - 1;
+  document.getElementById("count_" + id).innerText =  parseInt(document.getElementById("count_" + id).innerText, 10) - 1;
 }
 
 function transformMe(id, type) {
