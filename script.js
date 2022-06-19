24820f84fb01c3d83461681f3680f390d3854a0f
4eDo <61490588+4eDo@users.noreply.github.com>
2022-06-19 19:19:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -98,5 +98,5 @@ function updBoilerParams(type, addValue) {
    document.getElementById(textareaId).value = newStr;
 	
 	document.getElementById(barId).style.width = sum/count + "%";
-	document.getElementById(barId).title = sum/count + "%";
+	document.getElementById(barId+"_title").title = sum/count + "%";
 }
